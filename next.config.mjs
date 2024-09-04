444f87facef1d38f8d9926a03004c0c1d9226fb8
ECWireless <elliott@coopallc.com>
2024-09-04 15:22:23 -0600

    Add Push Protocol stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  env: {
+    PUBLIC_GROUP_CHAT_ID: process.env.PUBLIC_GROUP_CHAT_ID,
+  },
 };
 
 export default nextConfig;
